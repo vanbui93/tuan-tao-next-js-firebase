a00691aade75e193782f582ca95a20613829f353
Van Bui <van.bt.38@gmail.com>
2022-08-15 01:05:59 +0700

    file base trả về link

--- pages/dashboard/page/index.js
+++ pages/dashboard/page/index.js
@@ -29,7 +29,7 @@ import { deletePageDetail, getPageDetail, updatePageDetail } from './../../../st
 import { AdminStyle, StyledTableCell, StyledTableRow } from './../AdminStyle'
 import styles from './styles'
 import { storage } from './../../../utils/firebase'
-import { ref, uploadBytesResumable } from 'firebase/storage'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 
 import { EditorState } from 'draft-js'
 import dynamic from 'next/dynamic'
@@ -179,6 +179,48 @@ const AdminPage = props => {
 
         const imagesRef = ref(storage, `images/${file.name}`)
         const uploadTask = uploadBytesResumable(imagesRef, file)
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(
+            'state_changed',
+            snapshot => {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                console.log('Upload is ' + progress + '% done')
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused')
+                        break
+                    case 'running':
+                        console.log('Upload is running')
+                        break
+                }
+            },
+            error => {
+                // A full list of error codes is available at
+                // https://firebase.google.com/docs/storage/web/handle-errors
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break
+
+                    // ...
+
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect error.serverResponse
+                        break
+                }
+            },
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+                    console.log('File available at', downloadURL)
+                })
+            }
+        )
 
         return new Promise((resolve, reject) => {
             resolve({ data: { link: imageObject.localSrc } })
