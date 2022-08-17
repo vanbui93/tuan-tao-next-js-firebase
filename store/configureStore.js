2b7fd7c1708b0888bd68616bf2be1ec69675551b
Van Bui <vanbt@hivelab.com>
2022-08-17 16:42:42 +0700

    ẩn action trên production

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import { createWrapper } from 'next-redux-wrapper'
 import rootReducer from './reducers'
 import logger from 'redux-logger'
@@ -10,8 +10,7 @@ const initalState = {}
 const middleware = [thunk]
 
 if (process.env.NODE_ENV === 'development') {
-    middleware.push(logger),
-        typeof window !== 'undefined' && window.devToolsExtension ? window.devToolsExtension() : f => f
+    middleware.push(logger)
 }
 
 export const store = createStore(rootReducer, initalState, composeWithDevTools(applyMiddleware(...middleware)))
