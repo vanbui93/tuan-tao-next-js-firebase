c84fa958d7c7b4148b5cb270761bc635f7081d8c
Van Bui <vanbt@hivelab.com>
2022-08-19 14:30:19 +0700

    change theme color

--- layouts/LayoutUser/index.js
+++ layouts/LayoutUser/index.js
@@ -7,22 +7,22 @@ import Header from '../../components/Header'
 import { getMain } from '../../store/actions/main'
 
 function LayoutUser({ children }) {
-  const dispatch = useDispatch()
-  const mainData = useSelector(state => state.main.data)
-  useEffect(() => {
-    dispatch(getMain())
-  }, [])
-  return (
-    <div className='layout_user'>
-      <Header headerData={mainData} />
-      {children}
-      <div className='tel'>
-        <a href={`tel:${mainData.phone}`} className='tel-call'>
-          <FontAwesomeIcon icon={faPhone} style={{ fontSize: 25, color: '#fff' }} />
-        </a>
-      </div>
-      <Footer footerData={mainData} />
-    </div>
-  )
+    const dispatch = useDispatch()
+    const mainData = useSelector(state => state.main.data)
+    useEffect(() => {
+        dispatch(getMain())
+    }, [])
+    return (
+        <div className='layout_user'>
+            <Header headerData={mainData} />
+            {children}
+            <div className='tel'>
+                <a href={`tel:${mainData.phone}`} className='tel-call'>
+                    <FontAwesomeIcon icon={faPhone} style={{ fontSize: 25, color: '#fff' }} />
+                </a>
+            </div>
+            <Footer footerData={mainData} />
+        </div>
+    )
 }
 export default LayoutUser
